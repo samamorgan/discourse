6c33bd3ad6ea2b3fef40485836f621c36ff8190a
Sam Morgan <sam@plugable.com>
2019-09-15 22:28:50 -0700

    Added proper str and repr methods to JsonObject

--- discourse/jsonobject.py
+++ discourse/jsonobject.py
@@ -2,3 +2,15 @@ class JsonObject(object):
 
     def __init__(self, json):
         self.__dict__.update(json)
+
+    def __str__(self):
+        return '{} #{}'.format(self.__class__.__name__, self.id)
+
+    def __repr__(self):
+        vars_string = str(vars(self))
+
+        replace = {': ': '=', '{': '', '}': ''}
+        for key in replace:
+            vars_string = vars_string.replace(key, replace[key])
+
+        return '{}({})'.format(self.__class__.__name__, vars_string)
