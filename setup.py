8ccb73238ae05180ee7432038ec0e9eecba9165f
Jerome Leclanche <jerome@leclan.ch>
2019-10-20 15:50:42 +0200

    Use setup.cfg for package description

--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-import setuptools
+from setuptools import setup
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name='discourse',
-    version='0.1.2',
-    author='Sam Morgan',
-    author_email='sama4mail@gmail.com',
-    description='A Python wrapper of the Discourse API',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/samamorgan/discourse',
-    packages=setuptools.find_packages(),
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: OS Independent',
-        'Development Status :: 3 - Alpha',
-    ],
-    install_requires='requests',
-)
+setup(long_description_content_type="text/markdown")
