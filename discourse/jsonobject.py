783727a75fe707b0f6c7e2fbaf438d2c284ac958
Jerome Leclanche <jerome@leclan.ch>
2019-10-20 16:58:17 +0200

    Use __getattribute__ override instead of updating JsonObject attributes
    This is a cleaner approach because it doesn't depend on the structure of
    the python object (= better supports subclassing), and retains the
    original JSON object, exposed in JsonObject._data in case it's needed.

--- discourse/jsonobject.py
+++ discourse/jsonobject.py
@@ -1,21 +1,17 @@
-import json
-
-
 class JsonObject:
     """An object constructed from a JSON response"""
 
     def __init__(self, json):
-        self.update_attributes(json)
+        self._data = json
+
+    def __getattribute__(self, name):
+        if name != "_data" and name in self._data:
+            return self.json[name]
+
+        return super().__getattribute__(name)
 
     def __str__(self):
         return "{} with ID: {}".format(self.__class__.__name__, self.id)
 
     def __repr__(self):
-        try:
-            as_json = json.dumps(self.__dict__)
-        except TypeError:
-            as_json = "<invalid JSON>"
-        return "{}(json={})".format(self.__class__.__name__, as_json)
-
-    def update_attributes(self, json):
-        self.__dict__.update(json)
+        return "{}(json={})".format(self.__class__.__name__, repr(self._data))
