c0f78ae1eb6110cf8a62febb2a5233e2edb4ba17
Sam Morgan <sam@plugable.com>
2019-10-23 23:57:07 -0700

    Revert changes to JsonObject
    Moved back to updating attributes of the base class with the incoming JSON dict. This enables classes to be properly inspected with vars().
    
    Changed repr back. It still fails eval(), but this is a closer step to a correct object representation than the proposed implementation.
    
    Made client a required incoming argument.

--- discourse/jsonobject.py
+++ discourse/jsonobject.py
@@ -1,17 +1,27 @@
 class JsonObject:
     """An object constructed from a JSON response"""
 
-    def __init__(self, json):
-        self._data = json
+    def __init__(self, client, json):
+        self.client = client
+        self.update_attributes(json)
 
-    def __getattribute__(self, name):
-        if name != "_data" and name in self._data:
-            return self.json[name]
+    def __repr__(self):
+        # FIXME: This fails eval()
+        vars_string = str(self.__dict__)
 
-        return super().__getattribute__(name)
+        replace = {": ": "=", "{": "", "}": ""}
+        for key in replace:
+            vars_string = vars_string.replace(key, replace[key])
+
+        return "{}({})".format(self.__class__.__name__, vars_string)
 
     def __str__(self):
         return "{} with ID: {}".format(self.__class__.__name__, self.id)
 
-    def __repr__(self):
-        return "{}(json={})".format(self.__class__.__name__, repr(self._data))
+    def update_attributes(self, json):
+        self.__dict__.update(json)
+
+    def json(self):
+        d = self.__dict__
+        del d["client"]
+        return d
