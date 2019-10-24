efa959e2820013eec1c51ef1b7551f8c7b0b4141
Sam Morgan <sam@plugable.com>
2019-10-24 00:41:00 -0700

    Most Group() methods implemented

--- discourse/group.py
+++ discourse/group.py
@@ -3,25 +3,37 @@ from .jsonobject import JsonObject
 
 class Group(JsonObject):
     def delete(self):
-        raise NotImplementedError
+        response = self.client._request(
+            "DELETE", "/admin/groups/{}.json".format(self.id)
+        )
+
+        if response["success"] == "OK":
+            return True
+        return False
 
     def get_members(self):
-        raise NotImplementedError
+        return self.client._request("GET", "/groups/{}/members.json".format(self.name))
 
     def add_users(self, usernames):
-        if type(usernames) is str:
-            if "," in usernames:
-                usernames = usernames.split(",")
-        raise NotImplementedError
+        if type(usernames) is list:
+            usernames = ",".join(usernames)
+
+        response = self.client._request(
+            "PUT",
+            "/groups/{}/members.json".format(self.id),
+            params={"usernames": usernames},
+        )
+
+        if response["success"] == "OK":
+            return True
+        return False
 
     def remove_users(self, usernames):
         # Need to understand the difference between:
         # - Remove user(s) from a group
         # - Remove group assigned to a user
-        if type(usernames) is str:
-            if "," in usernames:
-                usernames = usernames.split(",")
         raise NotImplementedError
 
     def update(self):
-        return NotImplementedError
+        # TODO: Test what this actually does. This must take some sort of input.
+        return self.client._request("PUT", "/groups/{name}.json")
