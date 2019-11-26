6591e18c1732a096d1d8836e0b30aacc35865fc1
Sam Morgan <sam@plugable.com>
2019-11-26 00:36:45 -0800

    Completely re-structure package
    - Client now Session
    - All API endpoints in models sub-module

---
+++ discourse/models/__init__.py
@@ -0,0 +1,29 @@
+from .category import Category, CategoryList
+from .group import Group
+from .grouped_search_result import GroupedSearchResult
+from .notification import Notification
+from .plugin import Plugin
+from .post import Post
+from .private_message import PrivateMessage
+from .site import Site
+from .tag import Tag, TagGroup
+from .topic import Topic
+from .user import User
+from .web_hook import WebHook
+
+__all__ = [
+    "Category",
+    "CategoryList",
+    "Group",
+    "GroupedSearchResult",
+    "Notification",
+    "Plugin",
+    "Post",
+    "PrivateMessage",
+    "Site",
+    "Tag",
+    "TagGroup",
+    "Topic",
+    "User",
+    "WebHook",
+]
